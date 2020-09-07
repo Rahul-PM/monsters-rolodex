3c7a9943503c6e2eb44c20e325fe93fea731ca30
Rahul P M <rahul.p@thinkpalm.com>
2020-09-07 21:31:29 +0530

    added everything

---
+++ src/components/card/card.test.js
@@ -0,0 +1,15 @@
+import { shallow, mount, render } from 'enzyme';
+import React from 'react';
+import Card from './card.components';
+
+it('expexts to render card component', () => {
+    // const mockMonsters = [
+    //     {
+    //         "id": 1,
+    //         "name": "Leanne Graham",
+    //         "username": "Bret",
+    //         "email": "Sincere@april.biz"     
+    //     }
+    // ]
+    // expect(shallow(<Card monster={mockMonsters}/>).length()).toEqual(1);
+})
