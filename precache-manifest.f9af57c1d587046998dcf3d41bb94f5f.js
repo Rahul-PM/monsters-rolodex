1f2597d9ca337558ba0a954d6f90852379eaf0d1
Rahul-PM <65809663+Rahul-PM@users.noreply.github.com>
2020-09-08 13:00:36 +0530

    Add files via upload

---
+++ precache-manifest.f9af57c1d587046998dcf3d41bb94f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e675a9eefd163b8e84b8dc2517e20fbd",
+    "url": "/monsters-rolodex-example/index.html"
+  },
+  {
+    "revision": "b49dffcaa801582f9a7d",
+    "url": "/monsters-rolodex-example/static/css/main.614ae163.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex-example/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-example/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49dffcaa801582f9a7d",
+    "url": "/monsters-rolodex-example/static/js/main.345be033.chunk.js"
+  },
+  {
+    "revision": "d68fdcf83fda8b266506",
+    "url": "/monsters-rolodex-example/static/js/runtime-main.9e3e1f1d.js"
+  }
+]);
